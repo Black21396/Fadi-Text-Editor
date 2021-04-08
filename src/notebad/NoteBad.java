ad76aac57226d9b80fbe1a041f86823144c2a019
Fadi Salameh <fadi.199620@gmail.com>
2021-04-08 20:48:10 +0200

    Fadi Text Editor

---
+++ src/notebad/NoteBad.java
@@ -0,0 +1,12 @@
+
+package notebad;
+
+
+import javax.swing.JOptionPane;
+
+public class NoteBad {
+
+    public static void main(String[] args) {
+        new notebadProject().setVisible(true);      
+     }
+}
